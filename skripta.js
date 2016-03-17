64bc1abb803d2880b604b8313c2dc48d42e731d8
BlaFab <bf7617@student.uni-lj.si>
2016-03-17 18:13:04 +0000

    Dinamika gumba za odstranjevanje barv

--- skripta.js
+++ skripta.js
@@ -13,6 +13,16 @@ window.addEventListener('load', function() {
 		.addEventListener('click', dodajBarvo);
 		
 	//Odstrani barve
+	var odstraniBarve = function(event) {
+		var odstranjevalec = document.getElementById("barve");
+		//odstranjevalec.parentNode.removeChild(input);			// po uporabi ne dela več gumb "dodajBarvo!"
+        while(odstranjevalec.firstChild) {
+        	odstranjevalec.removeChild(odstranjevalec.firstChild);
+        }
+	}	
+	
+	document.querySelector("#odstraniBarve") 
+		.addEventListener('click', odstraniBarve);
 	
 	//Stroboskop
 	var vrednosti = [];
